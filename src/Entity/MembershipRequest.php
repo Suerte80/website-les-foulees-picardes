ad2c5eb3667300599f65c6731e7f057ee5900f8e
Suerte80 <alexiscourtin3@gmail.com>
2025-09-10 09:48:31 +0200

    Ajout des premiers entité et du controller Membership

---
+++ src/Entity/MembershipRequest.php
@@ -0,0 +1,171 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MembershipRequestRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MembershipRequestRepository::class)]
+class MembershipRequest
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $email = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $firstName = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $lastName = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $message = null;
+
+    #[ORM\Column]
+    private ?bool $rgpdAccepted = null;
+
+    #[ORM\Column(length: 20)]
+    private ?string $status = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $updatedAt = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $reminderSendAt = null;
+
+    #[ORM\ManyToOne(inversedBy: 'membershipRequests')]
+    private ?Member $validatedBy = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): static
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): static
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(?string $message): static
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    public function isRgpdAccepted(): ?bool
+    {
+        return $this->rgpdAccepted;
+    }
+
+    public function setRgpdAccepted(bool $rgpdAccepted): static
+    {
+        $this->rgpdAccepted = $rgpdAccepted;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(?\DateTimeImmutable $updatedAt): static
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    public function getReminderSendAt(): ?\DateTimeImmutable
+    {
+        return $this->reminderSendAt;
+    }
+
+    public function setReminderSendAt(\DateTimeImmutable $reminderSendAt): static
+    {
+        $this->reminderSendAt = $reminderSendAt;
+
+        return $this;
+    }
+
+    public function getValidatedBy(): ?Member
+    {
+        return $this->validatedBy;
+    }
+
+    public function setValidatedBy(?Member $validatedBy): static
+    {
+        $this->validatedBy = $validatedBy;
+
+        return $this;
+    }
+}
