0a3d9ea9f4c9108d1e05c6b46967b074ee04bd04
Suerte80 <alexiscourtin3@gmail.com>
2025-09-12 08:01:41 +0200

    Ajout de la commande pour élevé le role d'un utilisateur.

--- src/Entity/Role.php
+++ src/Entity/Role.php
@@ -27,9 +27,19 @@ class Role
     #[ORM\ManyToMany(targetEntity: RolePermission::class, mappedBy: 'roleId')]
     private Collection $rolePermissions;
 
+    /**
+     * @var Collection<int, Member>
+     */
+    #[ORM\ManyToMany(targetEntity: Member::class, mappedBy: 'rolesEntities')]
+    private Collection $members;
+
+    #[ORM\Column(length: 25)]
+    private ?string $code = null;
+
     public function __construct()
     {
         $this->rolePermissions = new ArrayCollection();
+        $this->members = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -84,6 +94,45 @@ class Role
         if ($this->rolePermissions->removeElement($rolePermission)) {
             $rolePermission->removeRoleId($this);
         }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Member>
+     */
+    public function getMembers(): Collection
+    {
+        return $this->members;
+    }
+
+    public function addMember(Member $member): static
+    {
+        if (!$this->members->contains($member)) {
+            $this->members->add($member);
+            $member->addRolesEntity($this);
+        }
+
+        return $this;
+    }
+
+    public function removeMember(Member $member): static
+    {
+        if ($this->members->removeElement($member)) {
+            $member->removeRolesEntity($this);
+        }
+
+        return $this;
+    }
+
+    public function getCode(): ?string
+    {
+        return $this->code;
+    }
+
+    public function setCode(string $code): static
+    {
+        $this->code = $code;
 
         return $this;
     }
