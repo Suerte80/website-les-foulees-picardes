a2a23df5e04199fe7ababa0ff44e3bcebe8be91a
Suerte80 <alexiscourtin3@gmail.com>
2025-09-12 11:39:49 +0200

    Ajout d'une page 404 et du réglement intérieur.

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -6,13 +6,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/', name: 'app_')]
 final class MainController extends AbstractController
 {
-    #[Route('/main', name: 'app_main')]
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
         ]);
+    }
+
+    #[Route('/bylaws', name: 'bylaws')]
+    public function bylaws(): Response
+    {
+        return $this->render('main/bylaws.html.twig', []);
     }
 }
