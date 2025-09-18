dc311601f101c72b42d6f601379ebb36e5286328
Suerte80 <alexiscourtin3@gmail.com>
2025-09-18 09:23:52 +0200

    Ajout d'un système de fichier en arborescence.

--- src/Entity/FileItem.php
+++ src/Entity/FileItem.php
@@ -2,7 +2,10 @@
 
 namespace App\Entity;
 
+use App\Enum\FileItemType;
 use App\Repository\FileItemRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -16,6 +19,9 @@ class FileItem
 
     #[ORM\Column(length: 255)]
     private ?string $name = null;
+
+    #[ORM\Column(enumType: FileItemType::class)]
+    private FileItemType $type = FileItemType::FILE;
 
     #[ORM\Column(length: 255)]
     private ?string $path = null;
@@ -32,9 +38,19 @@ class FileItem
     #[ORM\Column]
     private ?\DateTimeImmutable $createdAt = null;
 
+    #[ORM\ManyToOne(targetEntity: self::class, inversedBy: 'children')]
+    private ?self $parent = null;
+
+    /**
+     * @var Collection<int, self>
+     */
+    #[ORM\OneToMany(targetEntity: self::class, mappedBy: 'parent', orphanRemoval: true)]
+    private Collection $children;
+
     public function __construct()
     {
         $this->createdAt = new \DateTimeImmutable();
+        $this->children = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -112,5 +128,81 @@ class FileItem
         $this->createdAt = $createdAt;
 
         return $this;
+    }
+
+    public function getType(): ?FileItemType
+    {
+        return $this->type;
+    }
+
+    public function setType(?FileItemType $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getParent(): ?self
+    {
+        return $this->parent;
+    }
+
+    public function setParent(?self $parent): static
+    {
+        $this->parent = $parent;
+
+        return $this;
+    }
+
+    public function getFileItem(): ?self
+    {
+        return $this->fileItem;
+    }
+
+    public function setFileItem(?self $fileItem): static
+    {
+        $this->fileItem = $fileItem;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, self>
+     */
+    public function getChildren(): Collection
+    {
+        return $this->children;
+    }
+
+    public function addChild(self $child): static
+    {
+        if (!$this->children->contains($child)) {
+            $this->children->add($child);
+            $child->setFileItem($this);
+        }
+
+        return $this;
+    }
+
+    public function removeChild(self $child): static
+    {
+        if ($this->children->removeElement($child)) {
+            // set the owning side to null (unless already changed)
+            if ($child->getFileItem() === $this) {
+                $child->setFileItem(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function isFolder(): bool
+    {
+        return $this->type === FileItemType::DIRECTORY;
+    }
+
+    public function isFile(): bool
+    {
+        return $this->type === FileItemType::FILE;
     }
 }
