aa61e74ac0289769c7d96b189b0bc9f886b31d96
Suerte80 <alexiscourtin3@gmail.com>
2025-09-30 17:46:33 +0200

    Ajout du support des flash dans javascript.

--- assets/js/flash.js
+++ assets/js/flash.js
@@ -17,3 +17,32 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+export function createFlashMessage(message, level = 'info')
+{
+    // Récupération de la template du message flash.
+    const flashTemplate = document.getElementById('flash-message');
+
+    // Clone du contenu de la template
+    const flashMessage = flashTemplate.content.firstElementChild.cloneNode(true);
+
+    // Ajout des éléments dans du message flash.
+    flashMessage.classList.add('bg-flash-' + level);
+    flashMessage.querySelector('p').innerText = message;
+
+    // Ajout des events sur les messages flash.
+    const closeBtn = flashMessage.querySelector('img');
+
+    closeBtn.addEventListener('click', function() {
+        flashMessage.remove();
+    })
+
+    setTimeout(() => {
+        if(flashMessage)
+            flashMessage.remove();
+    }, 5000);
+
+    // Ajout de du message flash dans le conteneur.
+    const flashContainer = document.getElementById('flash-container');
+    flashContainer.appendChild(flashMessage);
+}
